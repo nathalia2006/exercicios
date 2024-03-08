ce4996f18dc59e60fc2ad2920b111ca355ce02ac
NAT5HÁLIA_ORSO <nathi.orso10@gmail.com>
2024-03-08 11:30:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//comando para exibir mensagem
+console.log("INTER CAMPEÃO DE TUDO")
+
+//exercicio de calculos
+
+const num1=20;
+const num2=654;
+
+//soma
+let soma = num1 + num2;
+
+console.log("O valor da soma é" + soma + " - dale");
